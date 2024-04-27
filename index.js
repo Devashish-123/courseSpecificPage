fa8e2811c0d4af1f0e199943b45381a2e13ce0f4
Devashish-123 <dnkute99@gmail.com>
2024-04-27 15:36:18 +0530

    testCommit

---
+++ index.js
@@ -0,0 +1,34 @@
+function btnIgniteClick(){
+    document.getElementById("igniteButton").style.backgroundImage="linear-gradient(to bottom, #000000, #2D2D2D)";
+    document.getElementById("igniteButton").style.color="#FFFFFF"
+    document.getElementById("igniteButton").style.border=" 0.5px solid #00000033";
+    document.getElementById("igniteButton").style.fontWeight="600";
+    document.getElementById("igniteLogo").src="./images/ignitLogo.svg"
+    document.getElementById("batchType1").innerHTML="Ignite"
+    document.getElementById("batchType2").innerHTML="Ignite"
+    
+
+
+    document.getElementById("intesiveButton").style.backgroundImage="linear-gradient(#FFFFFF, #FFFFFF)"
+    document.getElementById("intesiveButton").style.color="#888888"
+    document.getElementById("intesiveButton").style.border="0.5px solid #00000033";
+    document.getElementById("intesiveButton").style.fontWeight="500";
+    document.getElementById("intesiveLogo").src="./images/intesivLogo.svg";
+}
+
+function btnIntensiveClick(){
+    document.getElementById("intesiveButton").style.backgroundImage="linear-gradient(to bottom, #0D99FF, #52B6FF, #B5E0FF)";
+    document.getElementById("intesiveButton").style.color="#FFFFFF"
+    document.getElementById("intesiveButton").style.border="1.3px solid #B5E0FF";
+    document.getElementById("intesiveButton").style.fontWeight="600";
+    document.getElementById("intesiveLogo").src="./images/intesivDarkLogo.svg";
+    document.getElementById("batchType1").innerHTML="Intensive"
+    document.getElementById("batchType2").innerHTML="Intensive"
+ 
+
+    document.getElementById("igniteButton").style.backgroundImage="linear-gradient(#FFFFFF, #FFFFFF)"
+    document.getElementById("igniteButton").style.color="#878787"
+    document.getElementById("igniteButton").style.border="0.5px solid #00000033";
+    document.getElementById("igniteButton").style.fontWeight="500";
+    document.getElementById("igniteLogo").src="./images/ignitDarkLogo.svg"
+}
